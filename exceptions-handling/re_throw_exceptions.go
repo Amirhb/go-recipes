ac69a281a401fd68ce503cfb8f89d9434ef9003f
Amir Hossein Babaeian <babaeian456@gmail.com>
2023-03-27 14:10:49 +0330

    Add re-throw-exceptions recipe

---
+++ exceptions-handling/re_throw_exceptions.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+func sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, errors.New("square root of a negative number")
+	}
+	return math.Sqrt(x), nil
+}
+
+func sqrtSum(x float64, y float64) (float64, error) {
+	sqrtX, err := sqrt(x)
+	if err != nil {
+		return sqrtX,errors.New("Sqrt(x) error: " + err.Error())
+	}
+
+	sqrtY, err := sqrt(y)
+	if err != nil {
+		return sqrtY,errors.New("Sqrt(y) error: " + err.Error())
+	}
+
+	return sqrtX + sqrtY, nil
+}
+
+func main() {
+	sqrtSum, err := sqrtSum(16.0, -25.0)
+
+	fmt.Println(sqrtSum)
+	fmt.Println(err)
+}
