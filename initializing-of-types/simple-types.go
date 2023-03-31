81e7695ed101187275121d5e54f6706f9bbd5cfe
Amir Hossein Babaeian <babaeian456@gmail.com>
2023-03-31 18:24:02 +0330

    Add initializing-simple-types recipe

---
+++ initializing-of-types/simple-types.go
@@ -0,0 +1,23 @@
+package main
+
+import "math"
+
+func main() {
+	number, otherNumber := 42, 37
+	const maxInt64 = math.MaxInt64
+	const Mb = 1048576
+
+	const exp = 2.71828
+	var billion = 1E+9
+
+	const pi = 3.14
+
+	const greeting = "Hello"
+
+	var text = `this is some multiline text`
+
+	const sunIsStar = true
+	var earthIsStar = false
+
+	const charA = 'A' //'\x41'
+}
